49a132d48c07ac2e393cb849f43da3efaa825278
ooing <ooing-0720@gmail.com>
2023-04-29 23:33:15 +0900

    first commit

---
+++ src/test/java/com/HDD/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
