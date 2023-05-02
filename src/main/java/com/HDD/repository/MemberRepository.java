50e6eb40033a7fa65f4fb7b8d1f08e4c6329e345
ooing <ooing-0720@gmail.com>
2023-05-02 21:30:09 +0900

    05-02 Repository를 Memory로 수정

--- src/main/java/com/HDD/repository/MemberRepository.java
+++ src/main/java/com/HDD/repository/MemberRepository.java
@@ -1,11 +1,11 @@
-package com.HDD.repository;
-
-import com.HDD.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Member findBySid(String sid);
-    Member findByEmail(String email);
-    Member findByNickname(String nickname);
-}
+//package com.HDD.repository;
+//
+//import com.HDD.member.Member;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface MemberRepository extends JpaRepository<Member, Long> {
+//
+//    Member findBySid(String sid);
+//    Member findByEmail(String email);
+//    Member findByNickname(String nickname);
+//}
